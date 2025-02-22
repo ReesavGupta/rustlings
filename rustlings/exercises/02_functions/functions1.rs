d10714725951b2b9a2ad1c60728c4790464db4d9
ReesvGupta <guptareesav16@gmail.com>
2025-02-22 22:49:06 +0530

    let's learn rustttttt :crab:

---
+++ rustlings/exercises/02_functions/functions1.rs
@@ -0,0 +1,7 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    print!("this is some function")
+}
+fn main() {
+    call_me(); // Don't change this line
+}
